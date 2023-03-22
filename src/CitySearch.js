754afd6fddfa7f866390b8098e74e876de73f25c
freak906 <119906147+nick-vns@users.noreply.github.com>
2023-03-22 14:25:37 -0600

    added alert messages

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,24 +1,39 @@
 import React, { Component } from "react";
+import { InfoAlert } from "./Alert";
 
 class CitySearch extends Component {
   state = {
     query: "",
     suggestions: [],
     showSuggestions: undefined,
+    infoText: "",
   };
 
   handleInputChanged = (event) => {
     const value = event.target.value;
+    this.setState({ showSuggestions: true });
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({ query: value, suggestions });
+
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        suggestions: [],
+        infoText:
+          "We can't find the city you're looking for. Please try another city.",
+      });
+    } else {
+      return this.setState({ query: value, suggestions, infoText: "" });
+    }
   };
 
   handleItemClicked = (suggestion) => {
     this.setState({
       query: suggestion,
+      suggestions: [],
       showSuggestions: false,
+      infoText: "",
     });
 
     this.props.updateEvents(suggestion);
@@ -32,24 +47,35 @@ class CitySearch extends Component {
           type="text"
           className="city"
           placeholder="Enter city"
+          value={this.state.query}
           onChange={this.handleInputChanged}
-          onFocus={() => {
-            this.setState({ showSuggestions: true });
+          style={
+            this.state.infoText && this.state.infoText.length > 0
+              ? { borderColor: "blue" }
+              : {}
+          }
+          onBlur={() => {
+            this.setState({ showSuggestions: false });
           }}
         />
+        <InfoAlert text={this.state.infoText} />
         <ul
           className="suggestions"
-          style={this.state.showSuggestions ? {} : { display: "none" }}
+          style={
+            this.state.showSuggestions && this.state.suggestions.length !== 0
+              ? {}
+              : { display: "none" }
+          }
         >
           {this.state.suggestions.map((suggestion) => (
             <li
               key={suggestion}
-              onClick={() => this.handleItemClicked(suggestion)}
+              onMouseDown={() => this.handleItemClicked(suggestion)}
             >
               {suggestion}
             </li>
           ))}
-          <li key="all" onClick={() => this.handleItemClicked("all")}>
+          <li key="all" onMouseDown={() => this.handleItemClicked("all")}>
             <b>See all cities</b>
           </li>
         </ul>
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
+import { ErrorAlert } from "./Alert";
 
 class NumberOfEvents extends Component {
   state = {
     eventCount: 50,
+    errorText: "",
   };
 
   handleNumberInput = (event) => {
     const value = event.target.value;
-    this.props.updateEvents(undefined, value);
-    this.setState({ eventCount: value });
+    if (value > 0 && value < 60) {
+      this.props.updateEvents(undefined, value);
+      this.setState({ eventCount: value, errorText: "" });
+    } else {
+      this.setState({
+        errorText: "Select a number from 1 to 50",
+      });
+    }
   };
 
   render() {
@@ -20,7 +28,13 @@ class NumberOfEvents extends Component {
           className="number"
           value={this.state.eventCount}
           onChange={this.handleNumberInput}
+          style={
+            this.state.errorText && this.state.errorText.length > 0
+              ? { borderColor: "red" }
+              : {}
+          }
         />
+        <ErrorAlert text={this.state.errorText} />
       </div>
     );
   }
