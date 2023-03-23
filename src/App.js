559ebabfc468875417a83e2f93c895f9e2597264
freak906 <119906147+nick-vns@users.noreply.github.com>
2023-03-23 15:26:59 -0600

    update

--- src/App.js
+++ src/App.js
@@ -19,32 +19,20 @@ class App extends Component {
 
   async componentDidMount() {
     this.mounted = true;
-    if (navigator.onLine) {
-      const accessToken = localStorage.getItem("access_token");
-      const isTokenValid = (await checkToken(accessToken)).error ? false : true;
-      const searchParams = new URLSearchParams(window.location.search);
-      const code = searchParams.get("code");
-      this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-      if ((code || isTokenValid) && this.mounted) {
-        getEvents().then((events) => {
-          if (this.mounted) {
-            this.setState({
-              events: events.slice(0, this.state.eventCount),
-              locations: extractLocations(events),
-            });
-          }
-        });
-      } else {
-        getEvents().then((events) => {
-          if (this.mounted) {
-            this.setState({
-              showWelcomeScreen: false,
-              events: events.slice(0, this.state.eventCount),
-              locations: extractLocations(events),
-            });
-          }
-        });
-      }
+    const accessToken = localStorage.getItem("access_token");
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({
+            events: events.slice(0, this.state.eventCount),
+            locations: extractLocations(events),
+          });
+        }
+      });
     }
   }
 
