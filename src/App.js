a74be57b6b1136424ff49316e350c354e9dcfbf9
freak906 <119906147+nick-vns@users.noreply.github.com>
2023-03-23 16:10:15 -0600

    debugging offline mode

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
+import WelcomeScreen from "./WelcomeScreen";
 import { extractLocations, getEvents, checkToken, getAccessToken } from "./api";
 import { WarningAlert } from "./Alert";
-import WelcomeScreen from "./WelcomeScreen";
 import "./App.css";
 import "./nprogress.css";
 
@@ -19,22 +19,26 @@ class App extends Component {
 
   async componentDidMount() {
     this.mounted = true;
-    let offlineEvents;
-    const accessToken = localStorage.getItem("access_token");
     if (navigator.onLine) {
+      const accessToken = localStorage.getItem("access_token");
       const isTokenValid = (await checkToken(accessToken)).error ? false : true;
-      const searchParams = new URLSearchParams(window.location.search);
+      const searchParams = new URLSearchParams(window.localStorage.search);
       const code = searchParams.get("code");
       this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-      offlineEvents = (code || isTokenValid) && this.mounted;
-    } else {
-      offlineEvents = accessToken && this.mounted;
-      this.setState({ showWelcomeScreen: false });
-
-      if (offlineEvents) {
+      if ((code || isTokenValid) && this.mounted) {
         getEvents().then((events) => {
           if (this.mounted) {
             this.setState({
+              events: events.slice(0, this.state.eventCount),
+              locations: extractLocations(events),
+            });
+          }
+        });
+      } else {
+        getEvents().then((events) => {
+          if (this.mounted) {
+            this.setState({
+              showWelcomeScreen: false,
               events: events.slice(0, this.state.eventCount),
               locations: extractLocations(events),
             });
